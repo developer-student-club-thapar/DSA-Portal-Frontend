31bfe844cd7bc98c7f237208f18b146d24ad7fff
Jayita Lakhmani <jlakhmani_be22@thapar.edu>
2023-07-25 05:19:28 +0530

    Background, Heading and Logo Added

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,10 +1,16 @@
 import {Fragment} from 'react';
 import './Signup.css';
+import logo from "./Assets/logo.png";
+import bg from "./Assets/BG.png";
+import kid from "./Assets/Component.png";
 
 const Signup = () => {
   return (
     <Fragment>
-      <h1>Signup</h1>
+      <h1 className='heading'>Create Account</h1>
+      <img src={logo} alt="" className="Logo" />
+      <img src={bg} alt="" className="bg" />
+      <img src={kid} alt="" className="Kid" />
     </Fragment>
   );
 };
