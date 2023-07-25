c0188c15c18de487f3c2279d355a17be7a9524dc
Jayita Lakhmani <jlakhmani_be22@thapar.edu>
2023-07-26 03:19:26 +0530

    Redirecting Issue Fixed

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -6,35 +6,67 @@ import bg from "./Assets/BG.png";
 import kid from "./Assets/Component.png";
 
 const Signup = () => {
+
   const navigate = useNavigate();
 
+  const handleSubmit = event => {
+    event.preventDefault();
+    navigate('/leaderboard');
+  };
+  
   return (
     <Fragment>
       <h1 className="heading">Create Account</h1>
-      <form className="whole">
-        <label htmlFor="fname" className="form3">First Name</label>
-        <input type="text" id="fname" name="fname" required className="form form2" />
-        <label htmlFor="lname" className="form3 form4">Last Name</label>
-        <input type="text" id="lname" name="lname" required className="form form2"/>
+      <form onSubmit={handleSubmit} className="whole">
+        <label htmlFor="fname" className="form3">
+          First Name
+        </label>
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          required
+          className="form form2"
+        />
+        <label htmlFor="lname" className="form3 form4">
+          Last Name
+        </label>
+        <input
+          type="text"
+          id="lname"
+          name="lname"
+          required
+          className="form form2"
+        />
         <br />
-        <label htmlFor="email" className="form3 form5">Email</label>
+        <label htmlFor="email" className="form3 form5">
+          Email
+        </label>
         <br />
-        <input type="email" id="email" name="email" required className="form"/>
+        <input type="email" id="email" name="email" required className="form" />
         <br />
-        <label htmlFor="pwd" className="form3 form6">Password</label>
+        <label htmlFor="pwd" className="form3 form6">
+          Password
+        </label>
         <br />
-        <input type="password" id="pwd" name="pwd" required className="form"/>
+        <input type="password" id="pwd" name="pwd" required className="form" />
         <br />
-        <label htmlFor="pwd" className="form3 form7">Confirm Password</label>
+        <label htmlFor="pwd" className="form3 form7">
+          Confirm Password
+        </label>
         <br />
-        <input type="password" id="pwd" name="pwd" required className="form"/>
+        <input type="password" id="pwd" name="pwd" required className="form" />
+        <br />
+        <input type="submit" value="Create Account" className="Submit"></input>
         <br />
       </form>
       <img src={logo} alt="" className="Logo" />
       <img src={bg} alt="" className="bg" />
       <img src={kid} alt="" className="Kid" />
-      <button onClick={() => navigate("/leaderboard")} className="Submit">Create Account</button>
     </Fragment>
   );
 };
+
 export default Signup;
+
+
